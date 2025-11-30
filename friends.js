557e02ba9d590cbb89a9da83d65db59da672135a
Aydoswork06 <goekhan.aydo@icloud.com>
2025-11-30 20:19:34 +0100

    Update friends.js
    Freunde hinzufügen Funktion eingebaut

--- friends.js
+++ friends.js
@@ -1,1 +1,26 @@
+// Gast Account erstellen + merken
+export function guestLogin(username) {
+  if (!username) return alert("Gib einen Benutzernamen ein!");
+
+  const guestUser = {
+    name: username,
+    id: "guest_" + Date.now(),
+  };
+
+  // local speichern, damit du wieder rein kannst
+  localStorage.setItem("entwurf_user", JSON.stringify(guestUser));
+  window.location.reload();
+}
+
+// gespeicherten User abrufen
+export function getCurrentUser() {
+  const user = localStorage.getItem("entwurf_user");
+  return user ? JSON.parse(user) : null;
+}
+
+// logout wieder Gast zutritt ermöglichen
+export function logoutUser() {
+  localStorage.removeItem("entwurf_user");
+  window.location.reload();
+}
 
